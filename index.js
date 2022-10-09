4a13e425f8ca18aa209d61747e613a1fd5521679
tiffani <tiffani@casthaste.com>
2022-10-09 11:37:13 -0700

    Allow user input for playerSelection

--- index.js
+++ index.js
@@ -8,25 +8,25 @@ let items = ["Rock", "Paper", "Scissors"];
 // Create function for single round play between player and computer
 function playRound(playerSelection, computerSelection) {
     let result;
-    if (playerSelection.toUpperCase()===computerSelection.toUpperCase()) {
+    if (playerSelection===computerSelection.toUpperCase()) {
         result = "Tie game!";
-    } else if (playerSelection.toUpperCase()==="ROCK" && computerSelection==="Scissors") {
+    } else if (playerSelection==="ROCK" && computerSelection==="Scissors") {
         result = "You win! Rock beats Scissors.";
-    } else if (playerSelection.toUpperCase()==="ROCK" && computerSelection==="Paper") {
+    } else if (playerSelection==="ROCK" && computerSelection==="Paper") {
         result = "You lose! Paper beats Rock.";
-    } else if (playerSelection.toUpperCase()==="PAPER" && computerSelection==="Rock") {
+    } else if (playerSelection==="PAPER" && computerSelection==="Rock") {
         result = "You win! Paper beats Rock.";
-    } else if (playerSelection.toUpperCase()==="PAPER" && computerSelection==="Scissors") {
+    } else if (playerSelection==="PAPER" && computerSelection==="Scissors") {
         result = "You lose! Scissors beats Paper."
-    } else if (playerSelection.toUpperCase()==="SCISSORS" && computerSelection==="Paper") {
+    } else if (playerSelection==="SCISSORS" && computerSelection==="Paper") {
         result = "You win! Scissors beats Paper."
-    } else if (playerSelection.toUpperCase()==="SCISSORS" && computerSelection==="Rock") {
+    } else if (playerSelection==="SCISSORS" && computerSelection==="Rock") {
         result = "You lose! Rock beats Scissors."
     }    
     return result;
 }
 
-const playerSelection = "pApeR";
+const playerSelection = prompt("Rock, paper, or scissors?").toUpperCase();
 const computerSelection = getComputerChoice(items);
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
