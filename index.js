b67c14de1fa12aa07ee62d9e1fdf24eaf8c32da2
tiffani <tiffani@casthaste.com>
2022-10-08 22:28:39 -0700

    Create function for computer's play

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Create function for computer's play
+function getComputerChoice(items) {
+    return items[Math.floor(Math.random()*items.length)];
+}
+
+let items = ["Rock", "Paper", "Scissors"];
+console.log(getComputerChoice(items));
+
 // Create function for single round play between player and computer
 // Create function for a 5-round game
