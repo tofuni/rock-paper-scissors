d984a6520504035b20ea577625c2b011ea495d4d
Tiffani <112773709+tofuni@users.noreply.github.com>
2023-06-05 22:48:32 -0700

    Write pseudocode v1 for game

---
+++ index.js
@@ -0,0 +1,19 @@
+// Declare a function that represents the computer's play
+
+// Ask the player to input 'rock', 'paper', or 'scissors'
+// If the player inputs something else, then print 'Error! Please input rock, paper, or scissors.'
+// Else initialize RPS game
+
+// When a player inputs 'rock', 'paper', or 'scissors'
+// Initialize one round of RPS game against computer
+// If the player plays 'rock' and the computer plays 'scissors', then print 'You Win! Rock beats Scissors.'
+// If the player plays 'scissors' and the computer plays 'paper', then print 'You Win! Scissors beats Paper.'
+// If the player plays 'paper' and the computer plays 'rock', then print 'You Win! Paper beats Rock.'
+// If the player plays the same thing as the computer, then print 'It's a tie!'
+// Else the computer wins, print 'You Lose! Rock beats Scissors / Scissors beats Paper / Paper beats Rock.'
+
+// Initialize game of RPS game that is five rounds
+// After a round of RPS is over
+// If the player wins, then they earn one point
+// If the player loses, then the computer earns one point
+// After five rounds are over, declare winner of game
