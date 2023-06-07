c52e45c4c7dacfe89810da19bbbd18aaa2d4ca16
Tiffani <112773709+tofuni@users.noreply.github.com>
2023-06-06 21:10:17 -0700

    Add lowercase transformation of player choice to playRound

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function getComputerChoice() {
 const computerSelection = getComputerChoice();
 
 // Ask the player to input 'rock', 'paper', or 'scissors'
-let playerInput = prompt("Input 'rock', 'paper', or 'scissors'");
-let playerSelection = playerInput.toLowerCase();
+let playerSelection = prompt("Input 'rock', 'paper', or 'scissors'");
 
 // If the player inputs something else, then print 'Error! Please input rock, paper, or scissors.'
 // Else initialize RPS game
@@ -22,27 +21,26 @@ let playerSelection = playerInput.toLowerCase();
 // Initialize one round of RPS game against computer
 function playRound(playerSelection, computerSelection) {
     // If the player plays the same thing as the computer, then print 'It's a tie!'
-    if (playerSelection === computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a tie!";
         // If the player plays 'rock' and the computer plays 'scissors', then print 'You Win! Rock beats Scissors.'
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
         return "You Win! Rock beats Scissors.";
         // If the player plays 'scissors' and the computer plays 'paper', then print 'You Win! Scissors beats Paper.'
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
         return "You Win! Scissors beats Paper.";
         // If the player plays 'paper' and the computer plays 'rock', then print 'You Win! Paper beats Rock.'
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
         return "You Win! Paper beats Rock.";
         // Else the computer wins, print 'You Lose! Rock beats Scissors / Scissors beats Paper / Paper beats Rock.'
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
         return "You Lose! Rock beats Scissors.";
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
         return "You Lose! Scissors beats Paper.";
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
         return "You Lose! Paper beats Rock.";
     }
 }
-
 // Initialize game of RPS game that is five rounds
 // After a round of RPS is over
 // If the player wins, then they earn one point
