ccf996323e534fdff7cbd4f1326499931d0b581f
Tiffani <112773709+tofuni@users.noreply.github.com>
2023-06-06 22:58:38 -0700

    Add score counter for player and computer
    Since final function game() needs to declare winner after five rounds,
    a scoring system needs to be implemented.
    
    Set global variables computerScore and playerScore to 0 to start.
    Increment scores within playRound() respectively for computer and
    player depending on winner of round. Manually test via console.log()
    to see if scores track accurately.

--- index.js
+++ index.js
@@ -19,27 +19,62 @@ function playRound() {
     const computerSelection = getComputerChoice();
     // Ask the player to input 'rock', 'paper', or 'scissors'
     const playerSelection = prompt("Input 'rock', 'paper', or 'scissors'");
-    // If the player plays the same thing as the computer, then print 'It's a tie!'
+    // If the player plays the same thing as the computer
     if (playerSelection.toLowerCase() === computerSelection) {
+        // then print 'It's a tie!'
         return "It's a tie!";
-        // If the player plays 'rock' and the computer plays 'scissors', then print 'You Win! Rock beats Scissors.'
+        // If the player plays 'rock' and the computer plays 'scissors'
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+        // player earns a point
+        ++playerScore;
+        // and print 'You Win! Rock beats Scissors.'
         return "You Win! Rock beats Scissors.";
-        // If the player plays 'scissors' and the computer plays 'paper', then print 'You Win! Scissors beats Paper.'
+        // If the player plays 'scissors' and the computer plays 'paper'
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+        // player earns a point
+        ++playerScore;
+        // and print 'You Win! Scissors beats Paper.'
         return "You Win! Scissors beats Paper.";
-        // If the player plays 'paper' and the computer plays 'rock', then print 'You Win! Paper beats Rock.'
+        // If the player plays 'paper' and the computer plays 'rock'
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+        // player earns a point
+        ++playerScore;
+        // and print 'You Win! Paper beats Rock.'
         return "You Win! Paper beats Rock.";
-        // Else the computer wins, print 'You Lose! Rock beats Scissors / Scissors beats Paper / Paper beats Rock.'
+        // If the computer plays 'rock' and the player plays 'scissors'
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+        // computer earns a point
+        ++computerScore;
+        // and print 'You Lose! Rock beats Scissors.'
         return "You Lose! Rock beats Scissors.";
+        // If the computer plays 'rock' and the player plays 'scissors'
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+        // computer earns a point
+        ++computerScore;
+        // and print 'You Lose! Scissors beats Paper.'
         return "You Lose! Scissors beats Paper.";
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+        // computer earns a point
+        ++computerScore;
+        // and print 'You Lose! Paper beats Rock.'
         return "You Lose! Paper beats Rock.";
     }
 }
+
+// Set computer and player score trackers
+let computerScore = 0;
+let playerScore = 0;
+
+// Manual test to see if single round play and score counter can work multiple rounds
+console.log(playRound());
+console.log(computerScore);
+console.log(playerScore);
+console.log(playRound());
+console.log(computerScore);
+console.log(playerScore);
+console.log(playRound());
+console.log(computerScore);
+console.log(playerScore);
 
 // Initialize game of RPS game that is five rounds
 // After a round of RPS is over
